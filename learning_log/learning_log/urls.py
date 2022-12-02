3a776d471e578897926967a82f26974e347cc1dc
KrzysztofiniOWO <264055@student.pwr.edu.pl>
2022-12-02 15:57:14 +0100

    Commit

---
+++ learning_log/learning_log/urls.py
@@ -0,0 +1,9 @@
+"""Defines url patterns for learning logs"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
+    path('', include('learning_logs.urls')),
+]
